2b0816435031a2a59dd7258cd8e3a0a83f8460d0
Michell2912 <stheffany.carabantes@catolica.edu.sv>
2026-09-10 09:39:17 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+num1 = 5
+
+num2 = 10
+
+suma = num1 + num2
+
+palabra = "Hola mundo";
+
+    console.log(palabra);
+
+let elemento = document.querySelector('div')
+ elemento.style.backgroundColor = 'white';
+
+ console.log(elemento1)
+ let elemento1 = "Mensaje 1"
+ let elemento = "Mensaje 2"
---
+++ practica.js
@@ -0,0 +1,19 @@
+int = 225;
+float = 2.3;
+Boolean = true;
+String = "Hola";
+num = 258;
+
+palabra = "holis"
+
+console.log("El tipo de dato de la variable es " +   number);
+
+console.log("El tipo de dato de la variable es " + typeof num);
+
+let elementohtml = document.querySelector('body');
+
+document.body.appendChild(document.createElement('div'));
+let documentdiv = document.querySelector('div');
+documentdiv.style.backgroundColor = '##ffffff  ';
+
+elementohtml.textContent("El numero entero es " + int + " EL dato decimal es " + float + " El datp boooleano es " + Boolean + "El dato Striing es " + String);
